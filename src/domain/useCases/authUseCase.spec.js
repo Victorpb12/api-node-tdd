300a84d5d96bb09530a22c3fae22250153bd65e8
victorpb12 <victorpbaumg@gmail.com>
2024-08-30 11:18:41 -0300

    remove unnecessary ifs

--- src/domain/useCases/authUseCase.spec.js
+++ src/domain/useCases/authUseCase.spec.js
@@ -1,6 +1,6 @@
 const AuthUseCase = require("./authUseCase");
 
-const { MissingParamError, InvalidParamError } = require("../../utils/errors");
+const { MissingParamError } = require("../../utils/errors");
 
 const makeSut = () => {
   class LoadUserByEmailRepositorySpy {
@@ -43,18 +43,14 @@ describe("Auth UseCase", () => {
     const sut = new AuthUseCase();
     const promise = sut.auth("any_email@gmail.com", "any_password");
 
-    expect(promise).rejects.toThrow(
-      new MissingParamError("loadUserByEmailRepository")
-    );
+    expect(promise).rejects.toThrow();
   });
 
   test("Should throw if LoadUserByEmailRepository has no load method", async () => {
     const sut = new AuthUseCase({});
     const promise = sut.auth("any_email@gmail.com", "any_password");
 
-    expect(promise).rejects.toThrow(
-      new InvalidParamError("loadUserByEmailRepository")
-    );
+    expect(promise).rejects.toThrow();
   });
 
   test("Should return null if LoadUserByEmailRepository returns null", async () => {
