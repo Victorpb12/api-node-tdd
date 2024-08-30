7fd195012e89ab362c83938026a52533c18c2072
victorpb12 <victorpbaumg@gmail.com>
2024-08-30 09:26:53 -0300

    move generic erros to a utils layer

--- src/presentation/errors/index.js
+++ src/presentation/errors/index.js
@@ -1,11 +1,7 @@
-const MissingParamError = require("./missingParamError");
-const InvalidParamError = require("./invalidParamError");
 const ServerError = require("./serverError");
 const UnauthorizedError = require("./unauthorizedError");
 
 module.exports = {
-  MissingParamError,
-  InvalidParamError,
   ServerError,
   UnauthorizedError,
 };
--- src/presentation/routers/loginRouter.js
+++ src/presentation/routers/loginRouter.js
@@ -1,5 +1,5 @@
 const HttpResponse = require("../helpers/httpResponse");
-const { MissingParamError, InvalidParamError } = require("../errors");
+const { MissingParamError, InvalidParamError } = require("../../utils/errors");
 
 class LoginRouter {
   constructor(authUseCase, emailValidator) {
