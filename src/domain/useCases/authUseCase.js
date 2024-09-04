a78102536e47f5502f6eac6f222644b30645dc61
victorpb12 <victorpbaumg@gmail.com>
2024-09-04 09:57:39 -0300

    ensure authusecase returns an accesstoken if valid credentials are provided

--- src/domain/useCases/authUseCase.js
+++ src/domain/useCases/authUseCase.js
@@ -27,7 +27,8 @@ class AuthUseCase {
       return null;
     }
 
-    await this.tokenGenerator.generate(user.id);
+    const accessToken = await this.tokenGenerator.generate(user.id);
+    return accessToken;
   }
 }
 
