78203d10023b1d22938b9ca2b25fdd8cd8303656
victorpb12 <victorpbaumg@gmail.com>
2024-10-17 16:15:07 -0300

    enable CORS

--- src/main/config/setup.test.js
+++ src/main/config/setup.test.js
@@ -4,9 +4,19 @@ const app = require("./app");
 describe("App Setup", () => {
   test("Should disable x-powered-by header", async () => {
     app.get("/test-x-powered-by", (req, res) => {
-      res.send("Teste");
+      res.send("Teste powered");
     });
-    const res = await request(app).get("/");
+    const res = await request(app).get("/test-x-powered-by");
     expect(res.headers["x-powered-by"]).toBeUndefined();
+  });
+
+  test("Should enable CORS", async () => {
+    app.get("/test-cors", (req, res) => {
+      res.send("Teste cors");
+    });
+    const res = await request(app).get("/test-cors");
+    expect(res.headers["access-control-allow-origin"]).toBe("*");
+    expect(res.headers["access-control-allow-methods"]).toBe("*");
+    expect(res.headers["access-control-allow-headers"]).toBe("*");
   });
 });
