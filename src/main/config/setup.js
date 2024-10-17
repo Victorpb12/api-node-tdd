44ad9e1c6a50297fa4ce8d95b33421a2ddbdaf7b
victorpb12 <victorpbaumg@gmail.com>
2024-10-17 16:40:26 -0300

    ensure request parse body as JSON

--- src/main/config/setup.js
+++ src/main/config/setup.js
@@ -1,9 +1,8 @@
+const cors = require("../middlewares/cors");
+const jsonParser = require("../middlewares/jsonParser");
+
 module.exports = (app) => {
   app.disable("x-powered-by");
-  app.use((req, res, next) => {
-    res.set("access-control-allow-origin", "*");
-    res.set("access-control-allow-methods", "*");
-    res.set("access-control-allow-headers", "*");
-    next();
-  });
+  app.use(cors);
+  app.use(jsonParser);
 };
