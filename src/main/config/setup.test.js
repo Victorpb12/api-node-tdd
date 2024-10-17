56ea83baf268758df2d86992e0ca5b3beb1ca24e
victorpb12 <victorpbaumg@gmail.com>
2024-10-17 16:55:57 -0300

    ensure request return content-type as json by default

--- src/main/config/setup.test.js
+++ src/main/config/setup.test.js
@@ -3,20 +3,11 @@ const app = require("./app");
 
 describe("App Setup", () => {
   test("Should disable x-powered-by header", async () => {
-    app.get("/test-x-powered-by", (req, res) => {
-      res.send("Teste powered");
+    app.get("/test_x_powered_by", (req, res) => {
+      res.send("");
     });
-    const res = await request(app).get("/test-x-powered-by");
-    expect(res.headers["x-powered-by"]).toBeUndefined();
-  });
 
-  test("Should enable CORS", async () => {
-    app.get("/test-cors", (req, res) => {
-      res.send("Teste cors");
-    });
-    const res = await request(app).get("/test-cors");
-    expect(res.headers["access-control-allow-origin"]).toBe("*");
-    expect(res.headers["access-control-allow-methods"]).toBe("*");
-    expect(res.headers["access-control-allow-headers"]).toBe("*");
+    const res = await request(app).get("/test_x_powered_by");
+    expect(res.headers["x-powered-by"]).toBeUndefined();
   });
 });
