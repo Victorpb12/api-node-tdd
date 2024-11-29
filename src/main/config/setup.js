56908db2fdfbe1c703bf1e56c3ae86758ff81e02
victorpb12 <victorpbaumg@gmail.com>
2024-11-29 16:47:39 -0300

    add routes file

--- src/main/config/setup.js
+++ src/main/config/setup.js
@@ -1,6 +1,6 @@
 const cors = require("../middlewares/cors");
 const jsonParser = require("../middlewares/jsonParser");
-const contentType = require("../middlewares/content-type");
+const contentType = require("../middlewares/contentType");
 
 module.exports = (app) => {
   app.disable("x-powered-by");
--- src/main/index.js
+++ src/main/index.js
@@ -1,5 +1,9 @@
-const app = require('./config/app');
+const MongoHelper = require("../infra/helpers/mongoHelper");
+const env = require("./config/env");
 
-app.listen(5858, () => {
-  console.log("Server started on port 5858");
-});
+MongoHelper.connect(env.mongoUrl)
+  .then(() => {
+    const app = require("./config/app");
+    app.listen(5858, () => console.log("Server started on port 5858"));
+  })
+  .catch(console.error);
